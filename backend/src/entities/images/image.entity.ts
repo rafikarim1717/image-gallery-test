72ecb7bfa4f0a0d9e75ca33abdafeedbed6f8010
rafikarim1717 <rafikarim@yahoo.com>
2026-08-16 21:43:39 +0700

    feat: add Image entity with database structure

---
+++ backend/src/entities/images/image.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity()
+export class Image {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  category!: string; // nature, architecture, people, technology
+
+  @Column()
+  url!: string; // MinIO object URL or S3 key
+
+  @Column({ default: 0 })
+  clickCount!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  user!: User;
+
+  @Column()
+  userId!: string; // FK to User
+}
